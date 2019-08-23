c3df1065b6bfd5c3d1ed09dafdf70575848aa3c4
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-08-22 21:35:00 -0400

    Lecture

---
+++ data/migrations/20190822203522_fruit-schema.js
@@ -0,0 +1,17 @@
+//the changes we want to make
+exports.up = function(knex) {
+  //create a fruits table
+  //define the schema
+  return knex.schema.createTable("fruits", tbl => {
+      tbl.increments()
+      tbl.string("Name", 128).unique().notNullable()
+      tbl.decimal("avgWeightOz")
+      tbl.boolean("delicious")
+  })
+};
+
+//undoes the changes
+exports.down = function(knex) {
+  //drop the fruits table
+  return knex.schema.dropTableIfExists("fruits")
+};
